fd9fe381ec3b412740f2296020ebe42433bab0bf
Zy-Eir <zyeiradam@gmail.com>
2024-10-14 19:00:05 -0400

    Added more details in results

--- function.js
+++ function.js
@@ -1,25 +1,38 @@
 const searchBarForm = document.querySelector(".search-bar-form");
 const searchBar = document.querySelector(".search-bar");
+const searchBarToggle = document.querySelector(".searchbar-toggle-hidden");
 const displayArea = document.querySelector(".display-area");
 const card = document.querySelector(".card");
 const forecastArea = document.querySelector(".forecast-area");
+const todaysDetailsArea = document.querySelector(".todays-details-area");
+
+
+const testSearchBarForm = document.querySelector(".test-searchbar-form");
+const testSearchBar = document.querySelector(".test-searchbar");
+
+
+const cityList = document.querySelector(".cityList");
 
 
 
 
 
-searchBarForm.addEventListener("submit", async (event) => {
+
+/*  autocomplete practice
+searchBar.addEventListener("keyup", async (event) => {
   event.preventDefault(); // prevents refresh
-  
+
  
   // const query = searchBar.value.toLowerCase();
   
-  const city = searchBar.value;
+  const cityQuery = searchBar.value;
 
-  if(city){
+  if(cityQuery){
       try {
-        const weatherData = await getWeatherData(city); //fetch(url);
-        displayWeatherData(weatherData);
+        const listData = await weatherDataAutoComplete(cityQuery); //fetch(url);
+
+        autocompleteList(listData);
+      
       } 
        catch (error) {
         console.error(error);
@@ -29,12 +42,187 @@ searchBarForm.addEventListener("submit", async (event) => {
     else{
           errorDisplay("Please enter a city")
     }
+
+
+    // 
+  
+ //
+
+  
+   //searchBar.value = "";
+  // console.log(searchBar.value.toLowerCase());
+ 
   
   
-  searchBar.value = "";
+
+
+ 
+  
+});
+
+*/
+
+
+
+
+
+/* 
+async function weatherDataAutoComplete(cityQuery){
+
+  
+  const autocompleteSearch = `https://api.weatherapi.com/v1/search.json?key=5d88cfd5846743c1b27184227240306&q=${cityQuery}` //
+     response = await fetch(autocompleteSearch);
+    searchBar.value =  `${cityQuery.toLowerCase()}`;
+
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error("No results found");
+    }
+
+    // filter autocomplete
+    
+
+   return await response.json();
+
+  
+    
+}  
+*/
+
+/* // autocomplete practice
+function autocompleteList(listData){
+  
+  console.log(listData);
+  cityList.textContent= "";
+  
+  listData.forEach((details) => { 
+    const listItem = document.createElement("li");
+    listItem.classList.add("listitem");
+    const listCity = document.createElement("h2");
+    listCity.textContent= `${details.name}`;
+    const listRegion = document.createElement("h2");
+    listRegion.textContent= `${details.region}`;
+    const listCountry = document.createElement("h2");
+    listCountry.textContent= `${details.country}`;
+
+    listItem.append(listCity, listRegion, listCountry);
+    
+    cityList.append(listItem);
+
+    
+   
+   
+  });
+ 
+        
+    
+
+           
+            .map((cityQuery) => {
+              
+            
+              
+
+                return `
+                            <li class="locationitem"    >
+                              <h2>${cityQuery.name}, ${cityQuery.region}, ${cityQuery.country} </h2>
+                            
+                            </li>
+                        `
+              
+            })
+            .join('');
+            cityList.innerHTML = htmlString;
+          
+    
+}
+
+*/
+
+
+async function getWeatherData(cityQuery){
+
+  
+  const url = `https://api.weatherapi.com/v1/forecast.json?q=${cityQuery}&key=5d88cfd5846743c1b27184227240306&alerts=yes&aqi=yes&days=3` //
+     response = await fetch(url);
+    
+    searchBar.value = `${cityQuery}`;
+    
+    
+
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error("could not fetch weather data");
+    }
+
+    // filter autocomplete
+    
+
+   return await response.json();
+
+  
+    
+}  
+
+
+
+
+function hideSearchBar(){
+  searchBar.classList.add("searchbar-hidden")
+  searchBarToggle.classList.remove("searchbar-toggle-hidden") 
+  searchBarToggle.classList.add("searchbar-toggle-active")
+  searchBarForm.classList.add("search-bar-form-hidden")
+  
+} searchBarForm.addEventListener('submit', hideSearchBar)
+
+function toggleSearchBar(){
+  searchBar.classList.remove("searchbar-hidden")
+  searchBarToggle.classList.remove("searchbar-toggle-active")
+  searchBarToggle.classList.add("searchbar-toggle-hidden")
+  searchBarForm.classList.remove("search-bar-form-hidden")
+} searchBarToggle.addEventListener('click', toggleSearchBar)
+
+
+searchBarForm.addEventListener("submit", async (event) => {
+  event.preventDefault(); // prevents refresh
+
+ 
+   
+ 
+  // const query = searchBar.value.toLowerCase();
+  
+   cityQuery = searchBar.value;
+
+  if(cityQuery){
+      try {
+        const weatherData = await getWeatherData(cityQuery); //fetch(url);
+
+        displayWeatherData(weatherData);
+        
+      } 
+       catch (error) {
+        console.error(error);
+        errorDisplay(error)
+      }
+  }
+    else{
+          errorDisplay("Please enter a city")
+    }
+
+
+    // 
+  
+ //
+
+  
+   searchBar.value = "";
   // displayArea.innerHTML = ''; // clears old search results.
   
   
+
+
  
   
 });
@@ -42,55 +230,75 @@ searchBarForm.addEventListener("submit", async (event) => {
 
 
 
- async function getWeatherData(city){
- // this for 3-day forecast const url = `http://api.weatherapi.com/v1/forecast.json?q=${city}&key=5d88cfd5846743c1b27184227240306&days=3` // make sure to add https://
-  
-  
-  const url = `https://api.weatherapi.com/v1/forecast.json?q=${city}&key=5d88cfd5846743c1b27184227240306&days=3` //
-    const response = await fetch(url);
-    searchBar.value =  `${city}`;
-    console.log(response);
-
-    if (!response.ok) {
-      throw new Error("could not fetch weather data");
-    }
-
-    return await response.json();
-  
-    
-}  
-
 function displayWeatherData(data){
   
     console.log(data);
 
  
-  let {   current:  {feelslike_f, condition: {code, icon, text}},
-            location: {name, region},
+  let {   current:  {feelslike_f,  condition: {code, icon, text}, wind_mph, humidity, uv},
+            location: {name, region, localtime},
             forecast: {forecastday: [{day: {avgtemp_f,  maxtemp_f, mintemp_f, daily_chance_of_rain}, hour: {temp_f}}]}
     } = data;  
   
     forecastArea.innerHTML=''; // clears previous search results
 
+    
 
+
+    let forecastDate = document.createElement("p");
+    forecastDate.classList.add("forecast-date");
+ 
+
+  // Forecast-cards start
     data.forecast.forecastday.forEach((forecastdays) => { 
       
-   //  const date = forecastdays.date;
-
-     // const minmaxTemp =  document.createElement("p");
-     // minmaxTemp.classList.add("minmax");
-     // minmaxTemp.textContent = "Low: " + `${mintemp_f.toFixed(0)}°F` +  " High: " + `${maxtemp_f.toFixed(0)}°F`;
-
  
    
 
     let forecastCards = document.createElement("div");
     forecastCards.classList.add("forecast-cards");
 
+   // date formatting start
+
+
+
+   options = {
+   
+  
+    weekday: 'long',
    
 
-    const date = document.createElement("p");
-     date.textContent = forecastdays.date;
+
+
+   // dateStyle: 'long' // note: dateStyle is a catch-all for weekday, month, day, etc. It will overide other individual styles.
+    
+
+  };
+  
+  let forecastFormatting = new Date(forecastdays.date);  // gets the data from date
+
+
+
+  let forecastDate = document.createElement("p");
+  forecastDate.classList.add("forecast-date");
+  forecastDate.textContent = new Intl.DateTimeFormat("en-US", options).format(forecastFormatting); // 
+
+ // console.log(forecastFormatting);
+
+
+   // date formatting end
+
+ 
+
+
+
+
+       
+
+
+     
+
+    
      
      
      const minmaxTemp =  document.createElement("p");
@@ -111,7 +319,7 @@ function displayWeatherData(data){
 
      rainAndIcon.append(forecastDailyRain, forecastDayIcon)
     
-    forecastCards.append(date, rainAndIcon, minmaxTemp);
+    forecastCards.append(forecastDate, rainAndIcon, minmaxTemp);
 
      
 
@@ -121,9 +329,9 @@ function displayWeatherData(data){
    
      
     });
-  
-  
-   
+  // Forecast-cards end
+
+
 
 
         
@@ -133,11 +341,43 @@ function displayWeatherData(data){
 
   card.textContent= "";
   
+
+  options = {
+   
   
-  const cityName =  document.createElement("h1");
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric'
+
+
+
+   // dateStyle: 'long' // note: dateStyle is a catch-all for weekday, month, day, etc. It will overide other individual styles.
+    
+
+  };
+  
+  let currentforecastFormatting = new Date(localtime);  // gets the data from date
+
+
+
+  let currentForecastDate = document.createElement("p");
+  currentForecastDate.classList.add("current-forecast-date");
+  currentForecastDate.textContent = new Intl.DateTimeFormat("en-US", options).format(currentforecastFormatting); // 
+
+  console.log(currentforecastFormatting);
+  
+
+  
+  const cityName =  document.createElement("p");
   cityName.classList.add("city-name");
   cityName.textContent = `${name}` + ", " + `${region}` ;
 
+
+  const dateLocationWrapper =  document.createElement("div");
+  dateLocationWrapper.classList.add("date-location-wrapper");
+
+
+  dateLocationWrapper.append(cityName, currentForecastDate);
 
 
   const currentTempIconWrap = document.createElement("div");
@@ -195,16 +435,48 @@ function displayWeatherData(data){
  // cityExtraInfoWrap.append(cityDesc, highLow, cityFeels);
 
   
-  card.append( cityName, currentTempIconWrap, currentWeatherDescWrapper);
+  card.append(dateLocationWrapper, currentTempIconWrap, currentWeatherDescWrapper);
 
  
 
   displayArea.append(card);
 
+  todaysDetailsArea.textContent = "";
+
+  const uspaIndex = document.createElement("p");
+  uspaIndex.classList.add("uspaindex");
+  uspaIndex.textContent =  "AQI: " + data.current.air_quality["us-epa-index"]; // to access data that has a hyphen in it, use dot notation.
 
 
-  
+  const windmph = document.createElement("p");
+  windmph.classList.add("wind-mph");
+  windmph.textContent = "Wind-Speed: " + `${wind_mph}`  + "mph";
+
+
+  const uvIndex = document.createElement("p");
+  uvIndex.classList.add("uv");
+  uvIndex.textContent = "UV index: " + `${uv}`;
+
+
+
+ 
+
+
+
+  const Humidity = document.createElement("p");
+  Humidity.classList.add("humidity");
+  Humidity.textContent = "Humidity: " + `${humidity}` + "%";
+
+
+
+  todaysDetailsArea.append(uspaIndex, windmph, uvIndex, Humidity);
+
+ // console.log(data.current.air_quality["us-epa-index"]); 
 }
+
+
+
+
 
 function getWeatherEmoji(code){
  
@@ -244,6 +516,29 @@ function errorDisplay(message){
 
 
 
+/* text example of date formatting 
 
 
+const dayArr = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday']; // array we create that has the weekday names
+
+const monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',  'December']; // array we create that has the weekday names
+     
+const date = new Date("2024-4-15"); // input date 
+
+let day = dayArr[date.getDay()]; // 
+let month = monthArr[date.getMonth()]; // 
+let dayNumber = date.getDate(); // 
+
+const fullDate =  document.createElement("p");
+fullDate.classList.add("huhuh");
+fullDate.textContent = `${day}` + `${month}` + `${dayNumber}`;
+
+displayArea.append(fullDate);
+
+
+console.log(day + month + dayNumber);
+
+
+ note: the above example is old and ineffecient, use Intl.DateTimeFormat().format()
+ */
 
